f3ad629e2abb6822f093b802d74af9b06c86a437
Viacheslav Yefisko <v.yefisko@gmail.com>
2024-02-28 21:12:45 +0200

    Added Homework_5

---
+++ IT_Step/Homeworks/Homework_5/Task_1/source/main.cpp
@@ -0,0 +1,85 @@
+/*
+ ==============================================================================
+ Name        : Homework_5-Task_1
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Some number is entered by the user. Define the number of digits
+               in this number, their sum and average, and the number of zeros.
+ ==============================================================================
+ */
+
+#include <iostream>
+
+int countDigits(int num)
+{
+    int digitCount = 0;
+
+    while (num != 0)
+    {
+        digitCount++;
+        num /= 10;
+    }
+
+    return digitCount;
+}
+
+int sumDigits(int num)
+{
+    int digitSum = 0;
+
+    while (num != 0)
+    {
+        digitSum += num % 10;
+        num /= 10;
+    }
+
+    return digitSum;
+}
+
+float averageDigits(int num)
+{
+    return (float)sumDigits(num) / countDigits(num);
+}
+
+int countZeros(int num)
+{
+    int zeroCount = 0;
+
+    while (num != 0)
+    {
+        if (num % 10 == 0)
+        {
+            zeroCount++;
+        }
+
+        num /= 10;
+    }
+
+    return zeroCount;
+}
+
+int getIntFromUser()
+{
+    std::cout << "Enter integer number: ";
+    int userNumb;
+    std::cin >> userNumb;
+
+    return userNumb;
+}
+
+int main()
+{
+    int number = getIntFromUser();
+
+    std::cout << "\nNumber of digits : " << countDigits(number)
+              << "\nSum of digits    : " << sumDigits(number)
+              << "\nAverage of digits: " << averageDigits(number)
+              << "\nNumber of zeros  : " << countZeros(number)
+              << std::endl;
+
+    std::cout << "\n=====> DONE <=====\n"
+              << std::endl;
+
+    return 0;
+}
