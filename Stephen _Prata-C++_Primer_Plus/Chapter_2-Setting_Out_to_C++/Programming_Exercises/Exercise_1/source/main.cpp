a4569fbbed2bc82e4f84b5b2dac7bf301e9c85d0
Viacheslav Yefisko <v.yefisko@gmail.com>
2024-03-15 20:12:15 +0200

    Added S.Prata-Ch.2-Ex.1

---
+++ Stephen _Prata-C++_Primer_Plus/Chapter_2-Setting_Out_to_C++/Programming_Exercises/Exercise_1/source/main.cpp
@@ -0,0 +1,24 @@
+/*
+ ==============================================================================
+ Name        : S.Prata-C++_Primer_Plus-ch.2-ex.1
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Write a C++ program that displays your name and address (or if
+               you value your privacy, a fictitious name and address).
+ ==============================================================================
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Name   : Viacheslav\n"
+              << "Address: Planet Earth"
+              << std::endl;
+
+    std::cout << "\n=====> DONE <=====\n"
+              << std::endl;
+
+    return 0;
+}
