2179bf3e52d1fab24612363e46d0f8e1bd63531a
Viacheslav Yefisko <v.yefisko@gmail.com>
2024-03-23 12:29:49 +0200

    Added S.Prata-Ch.4-Ex.2

---
+++ Stephen _Prata-C++_Primer_Plus/Chapter_4-Compound_Types/Programming_Exercises/Exercise_2/source/main.cpp
@@ -0,0 +1,46 @@
+/*
+ ==============================================================================
+ Name        : S.Prata-C++_Primer_Plus-ch.4-ex.2
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Rewrite Listing 4.4, using the C++ string class instead of char
+               arrays.
+ ==============================================================================
+ */
+/*
+Listing 4.4 instr2.cpp
+// instr2.cpp -- reading more than one word with getline
+#include <iostream>
+int main()
+{
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+    cout << "Enter your name:\n";
+    cin.getline(name, ArSize); // reads through newline
+    cout << "Enter your favorite dessert:\n";
+    cin.getline(dessert, ArSize);
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+    return 0;
+}
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+    std::string name;
+    std::string dessert;
+    cout << "Enter your name:\n";
+    getline(cin, name);
+    cout << "Enter your favorite dessert:\n";
+    getline(cin, dessert);
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+    return 0;
+}
